465ffa3a0e5d4eccc549694a1b2a6bcb701f6dbc
Sagar <sagar.m@triveous.com>
2024-07-17 15:45:36 +0530

    fix: force rediret root to basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/dashboard',
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                basePath: false,
+                permanent: false
+            }
+        ]
+    },
     output: "standalone",
     images: {
         remotePatterns: [
